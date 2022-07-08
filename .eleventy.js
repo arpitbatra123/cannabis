ec9ddb42d98200332d24e790c096a8af89a2f07f
Arpit Batra <arpitbatra123@gmail.com>
2022-07-08 14:54:19 +0530

    Add initial files

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+const markdownLazyLoadImages = require('markdown-it-image-lazy-loading'),
+    markdownIt = require('markdown-it'),
+    markdownAttrs = require('markdown-it-attrs');
+
+module.exports = (eleventyConfig) => {
+    eleventyConfig.addPassthroughCopy('assets');
+    eleventyConfig.addPassthroughCopy('favicon.ico');
+    eleventyConfig.addPassthroughCopy('manifest.json');
+    eleventyConfig.addPassthroughCopy('_redirects');
+    eleventyConfig.addPassthroughCopy('admin');
+
+    eleventyConfig.setDataDeepMerge(true);
+
+    const options = {
+            html: true,
+            breaks: true,
+            linkify: false,
+            typographer: true
+        },
+        markdownEngine = markdownIt(options);
+
+    markdownEngine.use(markdownLazyLoadImages);
+    markdownEngine.use(markdownAttrs);
+
+    eleventyConfig.setLibrary('md', markdownEngine);
+
+    // For extra config options
+    // return {
+    // }
+};
---
+++ assets/main.js
@@ -0,0 +1,7 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+}
+
+function toggleSidebar() {
+    document.body.classList.toggle("sidebar-open");
+}
