d263ee37aed55879b223f7c7556f8259f789eacf
kcblaq <kcblack22@gmail.com>
2022-07-22 08:42:52 +0100

    Completed blog page

--- assets/main.js
+++ assets/main.js
@@ -10,3 +10,14 @@ function toggleTheme() {
 function toggleSidebar() {
   document.body.classList.toggle("sidebar-open");
 }
+
+let x = document.getElementById("searchbar");
+x.style.display = "none";
+x.style.transition = "all 2s ease-in";
+function toggleSearchBar() {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
