a9867fb18e067c64cb7bf65166a4abcb51ecb53c
Arpit Batra <arbatra@twilio.com>
2022-11-19 20:26:50 +0530

    Add toc plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,22 @@
-const markdownLazyLoadImages = require('markdown-it-image-lazy-loading'),
-    markdownIt = require('markdown-it'),
-    markdownAttrs = require('markdown-it-attrs'),
-    markdownItAnchor = require('markdown-it-anchor'),
-    eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const markdownLazyLoadImages = require("markdown-it-image-lazy-loading"),
+    markdownIt = require("markdown-it"),
+    markdownAttrs = require("markdown-it-attrs"),
+    markdownItAnchor = require("markdown-it-anchor"),
+    eleventyNavigationPlugin = require("@11ty/eleventy-navigation"),
+    eleventyPluginTOC = require("eleventy-plugin-toc");
+
 SIMILAR_ARTICLES_LIMIT = 4;
 
 module.exports = (eleventyConfig) => {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(eleventyPluginTOC);
 
-    eleventyConfig.addPassthroughCopy('assets');
-    eleventyConfig.addPassthroughCopy('favicon.ico');
-    eleventyConfig.addPassthroughCopy('manifest.json');
-    eleventyConfig.addPassthroughCopy('_redirects');
-    eleventyConfig.addPassthroughCopy('admin');
-    eleventyConfig.addPassthroughCopy('robots.txt');
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
+    eleventyConfig.addPassthroughCopy("manifest.json");
+    eleventyConfig.addPassthroughCopy("_redirects");
+    eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addPassthroughCopy("robots.txt");
 
     eleventyConfig.setDataDeepMerge(true);
 
@@ -21,7 +24,7 @@ module.exports = (eleventyConfig) => {
             html: true,
             breaks: true,
             linkify: false,
-            typographer: true
+            typographer: true,
         },
         markdownEngine = markdownIt(options);
 
@@ -29,55 +32,61 @@ module.exports = (eleventyConfig) => {
     markdownEngine.use(markdownAttrs);
     markdownEngine.use(markdownItAnchor, {
         permalink: true,
-        permalinkClass: 'direct-link',
-        permalinkSymbol: '#'
+        permalinkClass: "direct-link",
+        permalinkSymbol: "#",
     });
 
-    eleventyConfig.setLibrary('md', markdownEngine);
+    eleventyConfig.setLibrary("md", markdownEngine);
 
     // Receives tagged collection
-    eleventyConfig.addFilter('englishPostsOnly', function (collection) {
+    eleventyConfig.addFilter("englishPostsOnly", function (collection) {
         return collection.filter((post) => {
-            return post.data.tags.includes('postsEnglish');
+            return post.data.tags.includes("postsEnglish");
         });
     });
 
-    eleventyConfig.addFilter('italianPostsOnly', function (collection) {
+    eleventyConfig.addFilter("italianPostsOnly", function (collection) {
         return collection.filter((post) => {
-            return post.data.tags.includes('posts');
+            return post.data.tags.includes("posts");
         });
     });
 
-    eleventyConfig.addFilter('removeDefaultTags', function (tags) {
+    eleventyConfig.addFilter("removeDefaultTags", function (tags) {
         return tags.filter((tag) => {
-            return !['postsEnglish', 'posts'].includes(tag);
+            return !["postsEnglish", "posts"].includes(tag);
         });
     });
 
-    eleventyConfig.addFilter('filterRelated', (collection, currentTags, currentUrl) => {
-        let pagesWithCommonTags = collection.filter((page) => {
-            if (page.url == currentUrl) {
-                return false;
-            }
+    eleventyConfig.addFilter(
+        "filterRelated",
+        (collection, currentTags, currentUrl) => {
+            let pagesWithCommonTags = collection.filter((page) => {
+                if (page.url == currentUrl) {
+                    return false;
+                }
 
-            const first = page.data.tags,
-                second = currentTags,
-                common = first.filter((x) => second.includes(x));
-            return common.length > 1;
-        });
+                const first = page.data.tags,
+                    second = currentTags,
+                    common = first.filter((x) => second.includes(x));
+                return common.length > 1;
+            });
 
-        pagesWithCommonTags = pagesWithCommonTags.slice(0, SIMILAR_ARTICLES_LIMIT);
-        return pagesWithCommonTags;
-    });
+            pagesWithCommonTags = pagesWithCommonTags.slice(
+                0,
+                SIMILAR_ARTICLES_LIMIT
+            );
+            return pagesWithCommonTags;
+        }
+    );
 
-    eleventyConfig.addCollection('tagsListGlobal', (collectionApi) => {
+    eleventyConfig.addCollection("tagsListGlobal", (collectionApi) => {
         const tagsSet = new Set();
         collectionApi.getAll().forEach((item) => {
             if (!item.data.tags) return;
             item.data.tags.forEach((tag) => tagsSet.add(tag));
         });
         return Array.from(tagsSet).filter((tag) => {
-            return !['postsEnglish', 'posts'].includes(tag);
+            return !["postsEnglish", "posts"].includes(tag);
         });
     });
 
